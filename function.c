f0346d786417e1645d9b36217a80adea8384e431
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-18 13:38:43 -0700

    h

--- function.c
+++ function.c
@@ -38,6 +38,9 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+
 	while (*format)
 	{
 	if (*format == '%' && (format[1] == 'd' || format[1] == 'i'))
